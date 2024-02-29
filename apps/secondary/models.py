b15f21f0455db0a3355b414ac81b50a16839a099
Nurbolottop <erk1nbaevw2711@gmail.com>
2024-03-01 04:58:06 +0600

    Uods

--- apps/secondary/models.py
+++ apps/secondary/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.utils import timezone
 import instaloader
-from django.core.files.base import ContentFile
-from urllib.request import urlopen
 
 class InstagramPost(models.Model):
     post_url = models.URLField()
-    image = models.ImageField(upload_to='instagram_posts', blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
     comments = models.TextField(blank=True)
@@ -14,7 +12,7 @@ class InstagramPost(models.Model):
     def save(self, *args, **kwargs):
         if not self.pk:  # Если объект сохраняется впервые
             loader = instaloader.Instaloader()
-            loader.login("deviates.kg", "erk1nbaew")  # Ваше имя пользователя и пароль
+            loader.login("shus_dv", "erk1nbaew")  # Новые имя пользователя и пароль
             post = instaloader.Post.from_shortcode(loader.context, self.post_url.split('/')[-2])
             if not self.description:
                 self.description = post.caption
@@ -22,7 +20,6 @@ class InstagramPost(models.Model):
                 self.created_at = post.date_utc
             if not self.comments:
                 self.comments = '\n'.join([comment.text for comment in post.get_comments()])
-            if not self.image:
-                image_data = urlopen(post.url).read()
-                self.image.save(f'{self.pk}.jpg', ContentFile(image_data))
+            if not self.image_url:
+                self.image_url = post.url
         super().save(*args, **kwargs)
